0ccc2cebd5ef1e86d913b69ac96cca0c69cef351
GJSayers <gemma.sayers1@btinternet.com>
2021-03-01 16:09:36 +0000

    add food.html & making-friends.html  and associated anchors in index

--- lollaLingua.js
+++ lollaLingua.js
@@ -5,24 +5,20 @@
 
 // display language on chosen subject page:
 
-function () {
-    if (chosenlangfr) {
-       "qu'est-ce que vous voulez apprendre aujord'hui?"
-        
-    } else if (chosenlangit){ 
-        "Cosa vuoi imparare oggi?"
-    }
-}
-
 // spin subject button on hover:
 
-// onclick subject choice action:
+// onclick subject choice action - flip then clik through to page:
 
-// onclick subject choice action:
+// onclick subject page choice action - click card, card flips click card 2, card flips:
 
-// subject page:
+// if match congratulations:
 
- 
+// if match score point:
+
+// if not match better luck & missed attempt score:
+
+// when all matched = well done! & display score:
+
 
 
 
