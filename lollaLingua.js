c03ed23918d4b0891d38155c8475dd38165a5bc9
GJSayers <gemma.sayers1@btinternet.com>
2021-03-04 14:27:46 +0000

    create further test code on animals.html. Add logic instructions in js

--- lollaLingua.js
+++ lollaLingua.js
@@ -12,21 +12,16 @@ $("#lang-button-it").click
     $("#italian").show(1000);
 });
 
-// onclick subject page choice action - click card, card flips click card 2, card flips:  from https://medium.com/free-code-camp/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
-const cards = document.querySelectorAll('.game-card');
 
-function flipCard() {
-  this.classList.toggle('flip');
-}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
 
-}
+
 
 
 // ref from codeopen.io https://codepen.io/urilee/pen/EEOyPr. (deleted, re-visit page to re-instate)
 
 
+// onclick subject choice action - flip then clik through to page:
 
 
 // choose avatar:
@@ -35,15 +30,4 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 
 // spin subject button on hover:
 
-// onclick subject choice action - flip then clik through to page:
-
-
-
-// if match congratulations:
-
-// if match score point:
-
-// if not match better luck & missed attempt score:
-
-// when all matched = well done! & display score:
-)
+})
--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
+// onclick - click card, card flips click card 2, card flips:  from https://medium.com/free-code-camp/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
+
 const cards = document.querySelectorAll('.game-card');
 
 function flipCard() {
   this.classList.toggle('flip');
 }
 
+
+
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+
+// when two cards are flipped stop futher cards from being flipped
+
+// work out if cards are a match
+
+// if match congratulations:
+
+// if match score point:
+
+// if not match better luck & missed attempt score:
+
+// when all matched = well done! & display score:
