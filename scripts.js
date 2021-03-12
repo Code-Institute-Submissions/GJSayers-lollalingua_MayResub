5a192951d94c1ad4da4ec515452d2fcb0604b13d
GJSayers <gemma.sayers1@btinternet.com>
2021-03-12 19:04:16 +0000

    add console logs to solve shuffling feature - now working on game start

--- scripts.js
+++ scripts.js
@@ -2,25 +2,59 @@ $(document).ready(function(){
     $();
     // onclick - click card, card flips click card 2, card flips:  from https://medium.com/free-code-camp/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
 
+// choose dificulty level
 
+// start game (shuffle cards on load)
 
-var gameCards = Array.from(document.getElementsByClassName(".game-card"));
+startGame();
+
+function startGame(){
+    shuffleDeck();
+    // startTimer
+    // startClickCounter
+    // startPairCounter
+}
+
+function shuffleDeck() {
+   // const gameCards = Array.from(document.getElementsByClassName(".game-card"));
+    $(".game-card").each(function() {
+        let shuffledDeck = Math.floor(Math.random()* 21);
+        console.log(shuffledDeck);
+        console.log(this.style.order);    
+     this.style.order = shuffledDeck;
+      
+
+    },)
+
+ 
+}
+},)
+ //   })
+//for (let i = 0; i < gameCards.length; i++) {
+ //    let shuffledDeck = Math.floor(Math.random()*(gameCards.length - 1) + 1);
+ //    gameCards.style.order = shuffledDeck;
+// }
+
+validateCardFlip();
 
 // when clicked a card flips to hide back face (card-back) and show front face (card-front) face. 
-$(".game-card").each.on("click",function(){
+// rules - flip card only if:
+// 0 or 1 cards are already flipped
+// not all pairs have already been matched 
+// the card has not already been matched
+function validateCardFlip(gameCard){
+    if 
+        (!$(this).hasClass("flip") && !$(this).hasClass(".matched"));
+    
+$(".game-card").on("click",function(){
 $(this).addClass("flip"); 
-});
+})}
+
 //let frontCard = (".card-front");
 //let backCard = (".card-back");
-let cardOne,cardTwo;
-    const newLocal = ".game-card";
+//let cardOne,cardTwo;
+ //   const newLocal = ".game-card";
 //let cardsTurned = 0;
-
-
-
-
-
-
 
 //    $(".game-card").click(function() {
 //        $().off("click");
@@ -75,16 +109,6 @@ let cardOne,cardTwo;
  //  turnedCard +=1 
  // $(this).addClass("turned");
 
-
-
-
-
-
-
-
-
-
-
 // if match congratulations:
 
 //$(".matched").function() {
@@ -93,13 +117,8 @@ let cardOne,cardTwo;
     
 //}
 
-
-
 // if match score point:
 
 // if not match better luck & missed attempt score:
 
 // when all matched = well done! & display score:
-
-)}));
-})
